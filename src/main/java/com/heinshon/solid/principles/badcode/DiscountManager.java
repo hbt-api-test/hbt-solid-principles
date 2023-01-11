86a10d72a782e4ed73d82c765ef83589507bbc87
Jhon Carlos Solis Ochoa <jcsolis78@hotmail.com>
2023-01-11 10:33:12 -0500

    Mejor lectura de datos

--- src/main/java/com/heinshon/solid/principles/badcode/DiscountManager.java
+++ src/main/java/com/heinshon/solid/principles/badcode/DiscountManager.java
@@ -2,25 +2,35 @@ package com.heinshon.solid.principles.badcode;
 
 public class DiscountManager {
 
-    public double ApplyDiscount(double price, AccountStatus accountStatus, int timeOfHavingAccountInYears){
+    public double applyDiscount(double price, AccountStatus accountStatus, int timeOfHavingAccountInYears){
         double priceAfterDiscount = 0;
 
         double discountForLoyaltyInPercentage  = (timeOfHavingAccountInYears > 5) ? (double) 5/100 : (double) timeOfHavingAccountInYears/100;
 
-        if(accountStatus == AccountStatus.NotRegistered){
-            priceAfterDiscount = price;
+
+        switch (accountStatus){
+            case NotRegistered:
+                priceAfterDiscount = price;
+                break;
+            case SimpleCustomer:
+                priceAfterDiscount = (price - (0.1 * price));
+                priceAfterDiscount = priceAfterDiscount -
+                        (discountForLoyaltyInPercentage * priceAfterDiscount);
+                break;
+            case ValuableCustomer:
+                priceAfterDiscount = (0.7 * price);
+                priceAfterDiscount = priceAfterDiscount -
+                        (discountForLoyaltyInPercentage * priceAfterDiscount);
+                break;
+            case MostValuableCustomer:
+                priceAfterDiscount = (price - (0.5 * price));
+                priceAfterDiscount = priceAfterDiscount -
+                        (discountForLoyaltyInPercentage * priceAfterDiscount);
+                break;
+            default:
+                break;
         }
-        else if(accountStatus == AccountStatus.SimpleCustomer){
-            priceAfterDiscount = ((price - (0.1 * price)) - discountForLoyaltyInPercentage  * (price - (0.1 * price)));
-        }
-        else if (accountStatus == AccountStatus.ValuableCustomer)
-        {
-            priceAfterDiscount = (0.7 * price) - discountForLoyaltyInPercentage  * (0.7 * price);
-        }
-        else if (accountStatus == AccountStatus.MostValuableCustomer)
-        {
-            priceAfterDiscount = (price - (0.5 * price)) - discountForLoyaltyInPercentage  * (price - (0.5 * price));
-        }
+
 
         return priceAfterDiscount;
     }
